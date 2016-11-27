76b8fc79bc8abacd58ef4e4b9740da697f0cb23c
Chance Smith <chancesmithb@gmail.com>
2016-11-26 23:26:20 -0600

    adds lodash, adds top/bottom team members, adds text details/styles to header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import teamData from './team.json';
+let _ = require('lodash');
 
 class App extends Component {
   constructor(props) {
@@ -28,57 +29,104 @@ class App extends Component {
         highest = tmp;
       }
     }
-    return "Winning team: " + winningTeam + " at $" + highest.toFixed(2);
+    return "Team " + winningTeam + " at $" + highest.toFixed(2);
   }
 
-  showLowest(){
-    // There's no real number bigger than plus Infinity
-    // var lowest = Number.POSITIVE_INFINITY;
-    // var tmp;
-    // for (var i=myArray.length-1; i>=0; i--) {
-    // tmp = myArray[i].Cost;
-    //   if (tmp < lowest) lowest = tmp;
-    // }
-    // console.log(lowest);
-
-    return "Jeremy";
-  }
-
-  showTeams(){
+  storeTeamSalesTotals(){
     let teamsList = this.state.teams;
     let membersList = this.state.teamMembers;
 
     //loop through teams
-    for (var a = teamsList.length - 1; a >= 0; a--) {
+    for (let a = teamsList.length - 1; a >= 0; a--) {
       // loop through team members
-      for (var b = membersList.length - 1; b >= 0; b--) {
+      for (let b = membersList.length - 1; b >= 0; b--) {
         let salesList = membersList[b].giftcardSales
         // if team member is in same team
         if(teamsList[a].name === membersList[b].team) {
           // loop through sales of this team member
-          for (var c = salesList.length - 1; c >= 0; c--) {
+          for (let c = salesList.length - 1; c >= 0; c--) {
             // add sales up for this team
             teamsList[a].total += salesList[c].giftcardRevenue;
           }
         }
       }
-    }    
+    }
   }
 
-  totalTeam(){
+  showLowestSalesTeamMembers(limit){
+    let oldMembersList = this.state.teamMembers;
 
+    // order by ascending
+    let sortedMembersList = _.sortBy(oldMembersList, 'totalSales', function(n) {
+      return Math.sin(n);
+    });
+
+    // create your components
+    let topSalesMembers = sortedMembersList.slice(0, limit).map(function(member) {
+       return(
+          <p className="red">{member.name} w/ ${member.totalSales} (Team {member.team})</p>
+       );
+    });
+
+    return topSalesMembers;
+  }
+
+  showHighestSalesTeamMembers(limit){
+    let oldMembersList = this.state.teamMembers;
+
+    // order by descending
+    let sortedMembersList = _.sortBy(oldMembersList, 'totalSales', function(n) {
+      return Math.sin(n);
+    }).reverse();
+
+    // create your components
+    let topSalesMembers = sortedMembersList.slice(0, limit).map(function(member) {
+       return(
+          <p className="green">{member.name} w/ ${member.totalSales} (Team {member.team})</p>
+       );
+    });
+
+    return topSalesMembers;
+  }
+
+  totalTeamMemberSalesTotals(){
+    let membersList = this.state.teamMembers;
+    // loop through team members
+    for (let a = membersList.length - 1; a >= 0; a--) {
+      let salesList = membersList[a].giftcardSales
+      membersList[a].totalSales = 0;
+      // loop through sales of this team member
+      for (let b = salesList.length - 1; b >= 0; b--) {
+        // add sales up for this team
+        membersList[a].totalSales += salesList[b].giftcardRevenue;
+      }
+    }
   }
 
   render() {
+    this.storeTeamSalesTotals();
+    this.totalTeamMemberSalesTotals();
     return (
       <div className="App">
         <div className="App-header">
-          <h2>SKJackson Teams</h2>
+          <h2>SK Jackson Teams</h2>
+          <div className="details">
+            <p><strong>December Challenge:</strong></p>
+            <p>Team that sells $6,000 in gift cards...</p>
+          </div>
+          <div className="details">
+            <p><strong>Incentive:</strong></p>
+            <p>Gets 10¢ raise, any blue or red SK jacket/pull-over of choice.</p>
+          </div>
         </div>
-        <p className="App-intro">
-          {this.showTeams()}
-          {this.showHighestTeam()}
-        </p>
+        <div className="App-intro">
+          <h1>Winning Team</h1>
+          <p>{this.showHighestTeam()}</p>
+          <h1>Top 3</h1>
+          <p>{this.showHighestSalesTeamMembers(3)}</p>
+          <h1>Bottom 3</h1>
+          <p>{this.showLowestSalesTeamMembers(3)}</p>
+        </div>
       </div>
     );
   }
